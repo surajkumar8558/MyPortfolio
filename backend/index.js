342190d6687e1ca4445f15086c9fff9be9072389
Suraj Kumar <52989584+surajkumar8558@users.noreply.github.com>
2024-04-25 18:21:30 +0530

    Connected Frontend from Backend

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 8000;
+
+// app.use(cors());
+
+app.get("/api/jokes", (req, res) => {
+  const projectData = [
+    {
+      projectID: 1,
+      projectName: "Portfolio",
+      technologiesUsed: ["React", "NodeJS", "Tailwind", "Express", "MongoDB"],
+    },
+    {
+      projectID: 2,
+      projectName: "ChatApp",
+      technologiesUsed: [
+        "WebSocket",
+        "React",
+        "NodeJS",
+        "Tailwind",
+        "Express",
+        "MongoDB",
+      ],
+    },
+    {
+      projectID: 3,
+      projectName: "E-Commerce",
+      technologiesUsed: ["React", "NodeJS", "Tailwind", "Express", "MongoDB"],
+    },
+  ];
+  return res.send(projectData);
+});
+
+app.listen(PORT, () => {
+  console.log("App is listening on PORT: " + PORT);
+});
