9b98a9c988ac44cbe68cdf4b5da234f2690303e9
Suraj Kumar <52989584+surajkumar8558@users.noreply.github.com>
2024-05-12 11:53:21 +0530

    minor changes 5

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://surajportfolio-kqkw.onrender.com/",
+      "/api": "https://surajportfolio-kqkw.onrender.com",
     },
   },
   plugins: [react()],
